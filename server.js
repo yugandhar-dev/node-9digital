7de71c7b0aee8b7e508fdafa873133289b049a00
Yugandhar <yugandharp20@gmail.com>
2022-02-20 02:59:31 +1100

    throw error when invalid json

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buf, encoding) => {
+      try {
+        JSON.parse(buf);
+      } catch (e) {
+        res.status(400);
+        throw new Error('Could not decode request: JSON parsing failed');
+      }
+    },
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', require('./routes/showsRoutes'));
